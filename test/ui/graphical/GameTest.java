3cac3350a78903ff6de8ec4c0affc3977e188316
Alonso del Arte <alonso.delarte@gmail.com>
2025-04-07 21:08:02 -0400

    Added Random instance for the test package

--- test/ui/graphical/GameTest.java
+++ test/ui/graphical/GameTest.java
@@ -16,6 +16,8 @@
  */
 package ui.graphical;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -25,6 +27,8 @@ import static org.junit.Assert.*;
  * @author Alonso del Arte
  */
 public class GameTest {
+    
+    static final Random RANDOM = new Random();
     
     /**
      * Test of main method, of class Game.
